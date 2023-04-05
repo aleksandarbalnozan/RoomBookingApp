ccd1f828868dd7f72ed947624e14eb61a37dbc14
aleksandarbalnozan <balnozan@live.com>
2023-04-05 18:54:31 +0200

    Added test for Null Argument Exceotion, refactored codebase to pass the new test

--- RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -6,6 +6,13 @@ namespace RoomBookingApp.Core.Tests
 {
     public class RoomBookingRequestProcessorTest
     {
+        private RoomBookingRequestProcessor _processor;
+
+        public RoomBookingRequestProcessorTest()
+        {
+            _processor = new RoomBookingRequestProcessor();
+        }
+
         [Fact]
         public void Should_Return_Room_Booking_Response_With_Request_Values()
         {
@@ -17,16 +24,22 @@ namespace RoomBookingApp.Core.Tests
                 Date = new DateTime(2021, 10, 20)
             };
 
-            var processor = new RoomBookingRequestProcessor();
-
             // Act
-            RoomBookingResult result = processor.BookRoom(request);
+            RoomBookingResult result = _processor.BookRoom(request);
 
             // Assert
             Assert.NotNull(result);
             Assert.Equal(request.FullName, result.FullName);
             Assert.Equal(request.Email, result.Email);
             Assert.Equal(request.Date, result.Date);
+        }
+
+        [Fact]
+        public void Should_Throw_Exception_For_Null_Request()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => _processor.BookRoom(null));
+
+            Assert.Equal("bookingRequest", exception.ParamName);
         }
     }
 }
